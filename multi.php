a950f8bd2cb0edd7d1e753fa8cb46254cb606c72
rubisabel00 <rubisabel_00@hotmail.com>
2022-10-27 06:56:32 -0500

    first commit

---
+++ multi.php
@@ -0,0 +1,9 @@
+<?php
+    $num1 = $_POST['numero1'];
+    $num2 = $_POST['numero2'];
+    $multi = $num1*$num2;
+
+    echo $multi;
+
+
+?>
---
+++ variables.php
@@ -0,0 +1,16 @@
+<?php
+/*
+    suma +
+    resta -
+    multiplicación *
+    división /
+*/
+    $num1 = 3;
+    $num2 = 2;
+    $num3 = 7;   
+    $suma =$num1+$num2+$num3; 
+
+    echo "el numero ".$num1." + "." el numero ".$num2." + ". "el numero ".$num3." = ".$suma;
+
+
+?>
